b4c59b327f09f1a6f45a73099e929506a7da681e
Anastasia <www.nastya97@yandex.ru>
2023-07-02 20:57:49 +0300

    feat: deploy settings

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -34,12 +34,12 @@ function App() {
   /* -------------------- API ------------------- */
   const auth = new Auth({
     //тут будет ссылка на мой бэк
-    baseUrl: 'https://api.movies-explorer-dip.nomoredomains.rocks/',
+    baseUrl: 'https://api.movies-explorer-dip.nomoredomains.rocks',
   });
 
   const mainApi = new MainApi({
     //тут будет ссылка на мой бэк
-    url: 'https://api.movies-explorer-dip.nomoredomains.rocks/',
+    url: 'https://api.movies-explorer-dip.nomoredomains.rocks',
     headers: {
       'Content-Type': 'application/json',
       authorization: `Bearer ${localStorage.getItem('token')}`,
